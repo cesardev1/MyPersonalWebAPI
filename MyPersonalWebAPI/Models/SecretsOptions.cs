9e821bb9dc468bca01ce1f9bd6644775366a3610
Cesar Rodrigo Nieto Lopez <cesar_rnl@hotmail.com>
2024-02-18 00:07:16 -0600

    feat(secrets): add JWT token key and PostgreSQL connection string

--- MyPersonalWebAPI/Models/SecretsOptions.cs
+++ MyPersonalWebAPI/Models/SecretsOptions.cs
@@ -2,9 +2,11 @@
 {
     public class SecretsOptions
     {
-        public string? WhatsappPhoneId { get; set; }
-        public string? ApiKeyWhatsapp { get; set; }
-        public string? ApiKeyOpenIA { get; set; }
-        public string? AccessTokenWhatsapp { get; set; }
+        public string WhatsappPhoneId { get; set; }
+        public string ApiKeyWhatsapp { get; set; }
+        public string ApiKeyOpenIA { get; set; }
+        public string AccessTokenWhatsapp { get; set; }
+        public string JWTSecretKey { get; set; }
+        public string PostgreConnectionString { get; set; }
     }
 }
