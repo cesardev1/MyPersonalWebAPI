bed2cd9f57445efdc249244613763d4967ccc12b
Cesar Rodrigo Nieto Lopez <cesar_rnl@hotmail.com>
2024-03-10 22:39:46 -0600

    feat(database): add migration for whatsapp message table

---
+++ MyPersonalWebAPI/Migrations/20240302113029_addTableWhatsappMessage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MyPersonalWebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class addTableWhatsappMessage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "whatsAppMessages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MessageId = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Direction = table.Column<int>(type: "integer", maxLength: 10, nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    MessageText = table.Column<string>(type: "text", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_whatsAppMessages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_whatsAppMessages_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_whatsAppMessages_UserId",
+                table: "whatsAppMessages",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "whatsAppMessages");
+        }
+    }
+}
