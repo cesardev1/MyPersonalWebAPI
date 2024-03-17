20612bf751e0350648c699644623f0c214235b20
Cesar Rodrigo Nieto Lopez <cesar_rnl@hotmail.com>
2024-03-17 17:14:31 -0600

    refactor(WhatsappMessage): separeted service to connection database

---
+++ MyPersonalWebAPI/Services/WhatsappClound/WhatsappRepository/IWhatsAppMessageRepository.cs
@@ -0,0 +1,10 @@
+using MyPersonalWebAPI.Models;
+using MyPersonalWebAPI.Models.Whatsapp;
+
+namespace MyPersonalWebAPI.Services.WhatsappClound
+{
+    public interface IWhatsAppMessageRepository: IServiceBase<WhatsAppMessage>
+    {
+        Task<bool> SaveMessage(WhatsAppMessage message);
+    }
+}
---
+++ MyPersonalWebAPI/Services/WhatsappClound/WhatsappRepository/WhatsAppMessageRepository.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore.Storage;
+using MyPersonalWebAPI.Data;
+using MyPersonalWebAPI.Models;
+using MyPersonalWebAPI.Models.Whatsapp;
+using MyPersonalWebAPI.Services;
+ 
+namespace MyPersonalWebAPI.Services.WhatsappClound
+{
+    class WhatsAppMessageRepository: ServiceBase<WhatsAppMessage>,IWhatsAppMessageRepository
+    {
+        private readonly ILogger<WhatsAppMessageRepository> _logger;
+        public WhatsAppMessageRepository(DatabaseContext context,
+                                         ILogger<WhatsAppMessageRepository> logger):base(context)
+        {
+            _context=context;
+            _logger=logger;
+        }
+        
+        public async Task<bool> SaveMessage(WhatsAppMessage message)
+        {
+            using(var transaction = await _context.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    await _context.whatsAppMessages.AddAsync(message);
+                    await _context.SaveChangesAsync();
+                    await transaction.CommitAsync();
+                    return true;
+                }
+                catch (System.Exception ex)
+                {
+                    _logger.LogError(ex,ex.StackTrace);
+                    await transaction.RollbackAsync();
+                    return false;
+                }
+            }
+        }
+    }
+}
